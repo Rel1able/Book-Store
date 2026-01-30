583c6cad631fb7d1d5628c77b4b4da3b1ffe5098
Yaroslav <yaroslav.shebovovkk@gmail.com>
2026-01-30 11:23:38 +0100

    added register validation that checks whether account with email exists already

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,37 +1,49 @@
 import type { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import {prisma} from "../../lib/prisma"
+import { prisma } from "../../lib/prisma"
 
-async function register(req: Request, res: Response){
-    try{
-        const {email, password} = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await prisma.user.create({
-            data: {email, password: hashedPassword}
+async function register(req: Request, res: Response) {
+    try {
+        const { email, password } = req.body;
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email }
         });
-        res.json({id: user.id, email: user.email});
-    }catch(err: any){
-        res.status(400).json({error: err.message})
+
+        if (existingUser) {
+            return res.status(400).json({ error: "User with this email already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                email,
+                password: hashedPassword
+            }
+        });
+        res.json({ id: user.id, email: user.email });
+    } catch (err: any) {
+        res.status(400).json({ error: err.message })
     }
 };
 
-async function login(req: Request, res: Response){
-    try{
-        const {email, password} = req.body;
-        const user = await prisma.user.findUnique({where: {email}});
-        if (!user) return res.status(400).json({message: "Invalid credentials"});
+async function login(req: Request, res: Response) {
+    try {
+        const { email, password } = req.body;
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
         const isValid = await bcrypt.compare(password, user.password);
-        if (!isValid) return res.status(400).json({message: "Invalid credentials"});
+        if (!isValid) return res.status(400).json({ message: "Invalid credentials" });
 
-        const token = jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET as string, {
+        const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET as string, {
             expiresIn: "1h",
         });
-        res.json({token});
-    }catch(err: any){
-        res.status(400).json({error: err.message});
+        res.json({ token });
+    } catch (err: any) {
+        res.status(400).json({ error: err.message });
     }
 }
 
-export {register, login}
+export { register, login }
